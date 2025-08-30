c9da3fad013c89e81ba1ac2a69ad4749bc0fefcf
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2025-08-30 15:29:41 +0200

    add test TestFinderUser

--- service_food/src/test/java/com/service/TestFinderUsers.java
+++ service_food/src/test/java/com/service/TestFinderUsers.java
@@ -2,10 +2,13 @@ package com.service;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TestFinderUsers {
 
     @Test
     public void userIsFind() {
-        FinderUsers finderUsers = new FinderUsers();
+        boolean found = FinderUsers.findUsersByEmail("LauraGreen@gmail.com");
+        assertTrue(found, String.valueOf(true));
     }
 }
